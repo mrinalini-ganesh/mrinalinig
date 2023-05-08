87b53658e4b5abfb94122c67c7476127bd9548a1
mrinalini-ganesh <mrinalini.ganesh@gmail.com>
2023-05-08 12:03:56 -0700

    week4

--- week4/ping1.py
+++ week4/ping1.py
@@ -2,34 +2,23 @@
 # This script reads a file of IPs and names, pings each one using the pinglib library, and prints the results to the screen.
 # Mrinalini-20230505: Second version
 
-import sys
 import pinglib
+import sys
 
 def main():
-    # Check if the file argument is passed
-    if len(sys.argv) < 2:
-        print("Please provide a file of IPs and names as an argument.")
-        return
-
-    # Get the file name from argument
+    # Get filename from command line argument
     filename = sys.argv[1]
 
-    # Read IPs and names from file
-    with open(filename, "r") as f:
-        for line in f:
-            # Split the line into IP and name
-            fields = line.strip().split()
-            if len(fields) != 2:
-                print(f"Invalid line in {filename}: {line}")
-                continue
-            ip, name = fields
+    # Open file and read IP/DNS names
+    with open(filename, 'r') as file:
+        ipordns_list = file.readlines()
 
-            # Ping IP and get the time
-            result = pinglib.pingthis(ip)
+    # Ping each IP/DNS and print results
+    for ipordns in ipordns_list:
+        ipordns = ipordns.strip()  # Remove newline character
+        result = pinglib.pingthis(ipordns)
+        print(f"{result[0]}, {result[1]}" if result[1] != "Not Found" else f"{result[0]}, NotFound")
 
-            # Print result with 2 decimal places
-            print(f"{name}, {result[1]:.2f}" if result else f"{name}, Not Found")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
