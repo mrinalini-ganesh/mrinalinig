c94768daf4b2bd63d033cbe2ee06f809e025ca01
mrinalini-ganesh <mrinalini.ganesh@gmail.com>
2023-06-08 14:05:07 -0700

    week8

--- week8/monitor-updown.py
+++ week8/monitor-updown.py
@@ -3,55 +3,49 @@
 # Mrinalini-20230602: initial version
 
 import csv
-import subprocess
 import time
+from datetime import datetime  # Import the datetime module
+import subprocess
 
 def pingthis(ipordns):
     # Call ping command and capture output
     ping_output = subprocess.run(['ping', '-c', '1', ipordns], capture_output=True, text=True)
-    
+
     # Check if ping was successful
     if ping_output.returncode == 0:
-        # Parse ping time from output
-        time_str = ping_output.stdout.split('time=')[1].split(' ')[0]
-        time_ms = int(float(time_str))
-        
         # Return IP and status
-        return [ipordns, "up"]
+        return "up"
     else:
         # Return IP and status
-        return [ipordns, "down"]
+        return "down"
 
 def main():
-    # Open servers.csv file
-    with open('servers.csv', 'r') as csvfile:
-        # Create a reader object
-        reader = csv.reader(csvfile)
-        
-        # Skip the header row
-        next(reader)
-        
-        # Create updown.csv file
-        with open('updown.csv', 'w', newline='') as outputfile:
-            # Create a writer object
-            writer = csv.writer(outputfile)
-            
-            # Write the header row
-            writer.writerow(['Timestamp', 'Server IP', 'Type', 'Status'])
-            
-            # Iterate over the servers
-            for row in reader:
-                server_ip = row[1]
-                timestamp = time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-                
-                # Call pingthis function to get the status
-                result = pingthis(server_ip)
-                
-                # Write the result to updown.csv
-                writer.writerow([timestamp, server_ip, 'updown', result[1]])
-                
-                # Sleep for 10 seconds
-                time.sleep(10)
+    num_iterations = 4  # Number of times to loop the server checking process
+    
+    for _ in range(num_iterations):
+        with open('servers.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile)
+
+            with open('updown.csv', 'w', newline='') as outputfile:
+                writer = csv.writer(outputfile)
+                writer.writerow(['Timestamp', 'Server IP', 'Type', 'Status'])
+
+                for row in reader:
+                    if row:  # Check if row is not empty
+                        server_ip = row[0]
+                        timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]  # Use datetime.now() to get the current timestamp
+
+                        result = pingthis(server_ip)
+                        writer.writerow([timestamp, server_ip, 'updown', result])
+
+                    else:
+                        print("Invalid row format in servers.csv")
+
+        print("Iteration completed!")
+        time.sleep(10)
+    
+    # Print a message after the loop completes
+    print("Server checking completed!")
 
 if __name__ == '__main__':
     main()
